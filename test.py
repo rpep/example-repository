f04614e523fa5d5afb776b825e205c08583b3b0d
Ryan Pepper <ryan.pepper@soton.ac.uk>
2018-10-29 14:40:17 +0000

    Added an extended Coulomb function

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
 import math
 
+import numpy as np
+
+x = np.array([2.0, 3.0, 5.0])
+
 def potential(r):
-    return 1 / abs(r)
+    R = np.linalg.norm(r)
+
+    return 1 / R
+
+
+print("potential(x) = ", potential(x))
+
 
 
